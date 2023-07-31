c7c13e508a22f80f4639aebc887c3cfc46f9ed62
Abdus Sattar <sasorganicagro@gmail.com>
2023-08-01 02:04:57 +0600

    Avoid CORS issues when API is called from the frontend app.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+
+  resources :users, only: [:index, :show, :destroy]
+  resources :appointments, only: [:index, :show, :create, :update, :destroy]
+
 end
