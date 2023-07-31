6c15169aa4a2e6d324dedbfeed554fe227521be0
Tracy-miranja <barazatracy44@gmail.com>
2023-07-31 20:57:30 +0300

    Add device

--- db/migrate/20230731071612_create_appointments.rb
+++ db/migrate/20230731071612_create_appointments.rb
@@ -4,8 +4,6 @@ class CreateAppointments < ActiveRecord::Migration[7.0]
       t.datetime :appointment_date
       t.json :status
       t.json :location
-      t.references :doctor, null: false, foreign_key: true
-      t.references :patient, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_31_122340) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.datetime "appointment_date"
+    t.json "status"
+    t.json "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.decimal "age"
+    t.json "address"
+    t.string "role"
+    t.string "photo"
+    t.string "jti"
+    t.string "qualification"
+    t.text "description"
+    t.decimal "experiences"
+    t.datetime "available_from"
+    t.datetime "available_to"
+    t.decimal "consultation_fee"
+    t.decimal "rating"
+    t.string "specialization"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end
