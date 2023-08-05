3c3eecaddb5f7849d52c90ff3e9586da0cfbca5a
Abdus Sattar <sasorganicagro@gmail.com>
2023-08-05 21:10:18 +0600

    Add model spec

--- README.md
+++ README.md
@@ -5,4 +5,6 @@
   <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--jvDLhx0b--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/cpcr5w0kgl6j94tss7n9.png" alt="logo" width="140"  height="auto" />
   <br/>
    <img src="https://lucid.app/publicSegments/view/e680d048-cf13-4004-83ac-bfa23bf96c0d/image.png" alt="diagram" height="auto" />
+<a href="https://documenter.getpostman.com/view/26957552/2s9XxsVGHT"> API Documentation Here</a>
+
 </div>
--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,18 +1,49 @@
-# spec/models/appointment_spec.rb
-
 require 'rails_helper'
 
 RSpec.describe Appointment, type: :model do
-  describe 'associations' do
-    it { should belong_to(:doctor).class_name('User').with_foreign_key('doctor_id') }
-    it { should belong_to(:patient).class_name('User').with_foreign_key('patient_id') }
+  let(:patient) { create(:user, :patient) }
+  let(:doctor) { create(:user, :doctor) }
+  let(:patient_token) { Devise::JWT::TestHelpers.auth_headers({}, patient)['Authorization'] }
+  let(:doctor_token) { Devise::JWT::TestHelpers.auth_headers({}, doctor)['Authorization'] }
+
+  let(:appointment_params) do
+    {
+      appointment_date: '2023-08-10',
+      patient_id: patient.id,
+      doctor_id: doctor.id,
+      status: { active: true, expire: false, cancel: false },
+      location: {
+        street: '123 Main St',
+        state: 'CA',
+        city: 'Los Angeles',
+        zip_code: '90001'
+      }
+    }
   end
 
-  describe 'validations' do
-    it { should validate_presence_of(:appointment_date) }
-    it { should validate_presence_of(:doctor_id) }
-    it { should validate_presence_of(:patient_id) }
-    it { should validate_presence_of(:status) }
-    it { should validate_presence_of(:location) }
+  context 'validations' do
+    let(:appointment1) { Appointment.new(appointment_params) }
+    let(:appointment2) { Appointment.new(appointment_params) }
+
+    it 'is not valid when appointment date is nil' do
+      appointment1.appointment_date = nil
+      expect(appointment1).not_to be_valid
+    end
+
+    it 'is not valid when patient is not present' do
+      appointment1.patient_id = nil
+      expect(appointment1).not_to be_valid
+    end
+
+    it 'is not valid when doctor is not present' do
+      appointment1.doctor_id = nil
+      expect(appointment1).not_to be_valid
+    end
+
+    it 'is valid with all required attributes' do
+      expect(appointment1).to be_valid
+      expect(appointment2).to be_valid
+    end
   end
+
 end
