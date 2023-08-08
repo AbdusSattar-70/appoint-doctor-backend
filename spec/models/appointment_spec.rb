09881b212edcde8f29b70f715c4419a2d9c4c974
Tracy-miranja <barazatracy44@gmail.com>
2023-08-08 11:31:50 +0300

    Add test for doctors details

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -5,3 +5,18 @@ RSpec.describe Appointment, type: :model do
   let(:doctor) { create(:user, :doctor) }
   let(:patient_token) { Devise::JWT::TestHelpers.auth_headers({}, patient)['Authorization'] }
   let(:doctor_token) { Devise::JWT::TestHelpers.auth_headers({}, doctor)['Authorization'] }
+
+  let(:appointment_params) do
+    {
+      appointment_date: '2023-08-10',
+      patient_id: patient.id,
+      doctor_id: doctor.id,
+      status: { active: true, expire: false, cancel: false },
+      location: {
+        street: '123 Main St',
+        state: 'CA',
+        city: 'Los Angeles',
+        zip_code: '90001'
+      }
+    }
+  end
