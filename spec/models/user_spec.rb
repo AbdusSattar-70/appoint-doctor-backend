d1a5cd9a7c5b0be74f1e489deed2eafcf12a06ab
Tracy-miranja <barazatracy44@gmail.com>
2023-08-08 11:44:29 +0300

    Create test for role method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,3 +6,11 @@ RSpec.describe User, type: :model do
   describe 'role methods' do
     let(:user1) { create(:user, :patient) }
     let(:user2) { create(:user, :doctor) }
+
+    it 'defines role methods' do
+        expect(user1.super_admin?).to eq(false)
+        expect(user2.admin?).to eq(false)
+        expect(user2.doctor?).to eq(true)
+        expect(user1.patient?).to eq(true)
+      end
+    end
